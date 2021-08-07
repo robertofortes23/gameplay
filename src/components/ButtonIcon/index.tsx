7c9104474b9fc0e70cf258ac45b1c29748be7ec6
Roberto Fortes <robertbernd23@gmail.com>
2021-08-07 20:19:57 -0300

    Criando e estilizando componente ButtonIcon

---
+++ src/components/ButtonIcon/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+
+import DiscordImg from '../../assets/discord.png'
+import { styles } from './styles';
+
+export function ButtonIcon () {
+  return(
+    <>
+    <TouchableOpacity style={styles.container}>
+      <View style={styles.iconWrapper}>
+        <Image source={DiscordImg} style={styles.icon} />
+      </View>
+      <Text style={styles.title}>
+        Entrar com Discord
+      </Text>
+    </TouchableOpacity>
+    </>
+  )
+}
