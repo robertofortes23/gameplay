7f18d8cc26c1d05be79e164e7070ebdabf72f9e4
Roberto Fortes <robertbernd23@gmail.com>
2021-08-07 23:32:57 -0300

    Instalando fontes e app loading. Atualizando expo.

--- App.tsx
+++ App.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import { useFonts } from '@expo-google-fonts/inter';
+import { Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
+import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani'
+import AppLoading from 'expo-app-loading'
+
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }
+
   return (
     <View style={styles.container}>
         <StatusBar 
