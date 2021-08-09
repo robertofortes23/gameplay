8da59f1b8aa559b5b9e77f0de2a46f4b2ce4cc61
Roberto Fortes <robertbernd23@gmail.com>
2021-08-08 21:48:20 -0300

    Identação

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -3,15 +3,15 @@ export const theme = {
     primary: '#E51C44',
 
     secondary100: '#330a0a',
-    secondary90: '#3d0d0d',    
+    secondary90: '#3d0d0d',
     secondary80: '#470e0e',
     secondary85: '#470e0f',
-    secondary70: '#651b1b', 
+    secondary70: '#651b1b',
     secondary60: '#651b1b',
-    secondary50: '#751815', 
-    secondary40: '#661d1d', 
-    secondary30: '#cc4949', 
-   
+    secondary50: '#751815',
+    secondary40: '#661d1d',
+    secondary30: '#cc4949',
+
     overlay: 'rgba(0,0,0,0.7)',
     highlight: '#ABB1CC',
     heading: '#DDE3F0',
@@ -26,5 +26,5 @@ export const theme = {
     title500: 'Rajdhani_500Medium',
     text400: 'Inter_400Regular',
     text500: 'Inter_500Medium',
-  }
+  },
 };
