5c06ab649c89b700aea9cf01b029d55065b03bfc
Roberto Fortes <robertbernd23@gmail.com>
2021-08-12 19:19:48 -0300

    Substituindo TouchOpacity por RectButton

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -31,7 +31,6 @@ export function SignIn () {
         </Text>
         <ButtonIcon
           title="Entrar com Discord"
-          activeOpacity={0.7}
           onPress={handleSigin}
         />
       </View>
