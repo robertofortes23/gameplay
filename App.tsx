0d405ab4b3f023d55045ba5e14261f7d13ea47a3
Roberto Fortes <robertbernd23@gmail.com>
2021-08-12 16:13:08 -0300

    Instalando react navigation 5 e configuração de rotas

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@ import { Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani
 import AppLoading from 'expo-app-loading'
 
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { Home } from './src/screens/Home';
 import { Background } from './src/components/Background';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </Background>
   );
 }
