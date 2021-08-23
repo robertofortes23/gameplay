f6b9a9de905e32c7988eb60c1160f18ed1f957b7
Roberto Fortes <robertbernd23@gmail.com>
2021-08-23 11:26:26 -0300

    Create Home page

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,11 +7,18 @@ export const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
-      width: '100%',
-      paddingHorizontal: 24,
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginTop: getStatusBarHeight() + 26,
-      marginBottom: 42
-  }
+    width: "100%",
+    paddingHorizontal: 24,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: getStatusBarHeight() + 26,
+    marginBottom: 42,
+  },
+  content: {
+    marginTop: 42,
+  },
+  matches: {
+    marginTop: 24,
+    marginLeft: 24,
+  },
 });
