4038abe89214067cb9b4b20e210243334acc9d10
Roberto Fortes <robertbernd23@gmail.com>
2021-08-23 11:24:27 -0300

    Category component

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -13,18 +13,27 @@ type Props = RectButtonProps & {
   checked?: boolean;
 };
 
-export function Category({ title, icon: Icon, checked = false, ...rest }: Props) {
-  const { primary } = theme.colors;
+export function Category({ title, icon: Icon, checked = true, ...rest }: Props) {
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
 
   return (
     <RectButton {...rest}>
-      <LinearGradient style={styles.container} colors={[primary, primary]}>
-        <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
-          <View style={checked ? styles.checked : styles.check}>
+      <LinearGradient
+        style={styles.container}
+        colors={[secondary50, secondary70]}
+      >
+        <LinearGradient
+          style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+          colors={[checked ? secondary85 : secondary50, secondary40]}
+        >
+          <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
+            <View style={checked ? styles.checked : styles.checked} />
+
             <Icon width={48} height={48} />
+
+            <Text style={styles.title}>{title}</Text>
           </View>
-          <Text style={styles.title}>{title}</Text>
-        </View>
+        </LinearGradient>
       </LinearGradient>
     </RectButton>
   );
--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -4,30 +4,44 @@ import { theme } from '../../global/styles/theme'
 export const styles = StyleSheet.create({
   container: {
     width: 104,
-    height: 120,
-    justifyContent: "center",
-    alignItems: "center",
+    height: 140,
+    justifyContent: 'center',
+    alignItems: 'center',
     borderRadius: 8,
     marginRight: 8,
   },
   content: {
     width: 100,
-    height: 116,
-    backgroundColor: theme.colors.secondary40,
+    height: 136,
     borderRadius: 8,
-    justifyContent: 'space-between',
     alignItems: 'center',
-    paddingVertical: 7
-  },
-  checked: {
-
+    justifyContent: 'space-between',
+    paddingVertical: 20
   },
   check: {
-
+    position: "absolute",
+    top: 7,
+    right: 7,
+    width: 12,
+    height: 12,
+    backgroundColor: theme.colors.secondary100,
+    borderColor: theme.colors.secondary50,
+    borderWidth: 2,
+    borderRadius: 3,
+  },
+  checked: {
+    position: "absolute",
+    top: 7,
+    right: 7,
+    width: 10,
+    height: 10,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 3,
   },
   title: {
-    fontFamily: theme.fonts.title500,
+    fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
-    fontSize: 15
-  }
+    fontSize: 15,
+    marginTop: 15,
+  },
 });
