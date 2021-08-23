30f25dc43c190d881191ac95552a1232c1f52be7
Roberto Fortes <robertbernd23@gmail.com>
2021-08-23 11:25:21 -0300

    GuildIcon Component

---
+++ src/components/GuildIcon/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import { theme } from '../../global/styles/theme'
+
+export const styles = StyleSheet.create({
+  image: {
+    width: 54,
+    height: 54,
+    borderRadius: 6,
+  },
+});
